d0b0fc3a451b9e8782565f793cd05c1fcba3559e
Biagio <biagio.santori@gmail.com>
2021-01-06 14:13:57 +0000

    I broke something To be honest no clue what

--- commands/vent.js
+++ commands/vent.js
@@ -36,58 +36,61 @@ exports.run = async (client, message, args, level) => {
 
   // If there is no message then let the user know there should be.
   if (!args[0]) {
-    message.channel.send("Please provide text to send. For example `!vent Hello`")
-  }
-
-  // Create the vent message.
-  const ventMessage = args.join(" ");
-
-  console.log(args.join(" "));
-  // Shift the message along one.
-  //ventMessage.shift();
-
-  // Wait one second
-  client.wait(1000);
-
-  // This ignores the Unhandled Rejection if bot attempts to delete author's messages in DM
-  if (message.channel.type === 'text') {
     // Delete the message the user sent.
     message.delete();
-  }
+    // Tell him
+    message.author.send("Please provide text to send. For example `!vent Hello`")
+  } else {
+    // Create the vent message.
+    const ventMessage = args.join(" ");
 
-  // Check for worrying messages
-  for (var i = 0; i < activeWords.length; i++) {
-    if (message.content.includes(activeWords[i])) {
-      message.author.send("**If you are having suicidal thoughts please reach out to your country's suicide hotline.**\nAlways remember that there are phone numbers that you can call *24 hours a day, 7 days a week*, from any location.\nYou **are** worthy, you **are** loved and you will **always** be able to find assistance.\n**You are not alone**. Please reach out.\n \n:united_nations: International: `116 123`\nFor a bigger list visit <#381838613559902208>")
-      activeWordsFound = true
-      break;
+    console.log(args.join(" "));
+    // Shift the message along one.
+    //ventMessage.shift();
+
+    // Wait one second
+    client.wait(1000);
+
+    // This ignores the Unhandled Rejection if bot attempts to delete author's messages in DM
+    if (message.channel.type === 'text') {
+      // Delete the message the user sent.
+      message.delete();
     }
-  }
 
-  // Send the message to the webhook that posts it to #vents
-  ventSender.send(ventMessage + ' - Anonymous')
+    // Check for worrying messages
+    for (var i = 0; i < activeWords.length; i++) {
+      if (message.content.includes(activeWords[i])) {
+        message.author.send("**If you are having suicidal thoughts please reach out to your country's suicide hotline.**\nAlways remember that there are phone numbers that you can call *24 hours a day, 7 days a week*, from any location.\nYou **are** worthy, you **are** loved and you will **always** be able to find assistance.\n**You are not alone**. Please reach out.\n \n:united_nations: International: `116 123`\nFor a bigger list visit <#381838613559902208>")
+        activeWordsFound = true
+        break;
+      }
+    }
 
-  // Send the message to the webhook that posts it to #vents-log
-  ventRevealer.send("```asciidoc\nTIMESTAMP::" + timestamp +"\nAUTHOR:: " + message.author.tag + " (" + message.author.id + ")\n" + "MESSAGE:: "+ ventMessage + "\nVENT ID:: " + ventID + "\nHELPSENT:: " + activeWordsFound + "```")
+    // Send the message to the webhook that posts it to #vents
+    ventSender.send(ventMessage + ' - Anonymous')
 
-  // Create a random ID.
-  const styledVentID = "`"+ventID+"`" ;
+    // Send the message to the webhook that posts it to #vents-log
+    ventRevealer.send("```asciidoc\nTIMESTAMP::" + timestamp +"\nAUTHOR:: " + message.author.tag + " (" + message.author.id + ")\n" + "MESSAGE:: "+ ventMessage + "\nVENT ID:: " + ventID + "\nHELPSENT:: " + activeWordsFound + "```")
 
-  // Let the author know privately it has been sent.
-  message.author.send("<:BotListYes:" + yesEmoji + "> | Your message was sucessfully posted.\n*Posted at `" + prettyTime + "` (Vent ID: " + styledVentID + ")*")
+    // Create a random ID.
+    const styledVentID = "`"+ventID+"`" ;
+
+    // Let the author know privately it has been sent.
+    message.author.send("<:BotListYes:" + yesEmoji + "> | Your message was sucessfully posted.\n*Posted at `" + prettyTime + "` (Vent ID: " + styledVentID + ")*")
+  }  
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["tv"],
+  aliases: ["v"],
   permLevel: "User", // Who can use it
   cooldown: 5
 };
 
 exports.help = {
-  name: "testvent",
+  name: "vent",
   category: "Miscelaneous",
-  description: "Command used to test anonymous messages to #vents",
-  usage: "'testvent <message>'"
+  description: "Command used to send anonymous messages to #vents",
+  usage: "'vent <message>'"
 };
