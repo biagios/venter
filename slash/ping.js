5926e04efe9ba43714f734042ee8bf77d95e4ad5
Restyled.io <commits@restyled.io>
2024-03-21 14:38:44 +0000

    Restyled by clang-format

--- slash/ping.js
+++ slash/ping.js
@@ -3,22 +3,23 @@ exports.run = async (client, interaction) => {
   await interaction.deferReply();
   const reply = await interaction.editReply("Ping?");
   await interaction.editReply(
-    `Pong! Latency is ${
-      reply.createdTimestamp - interaction.createdTimestamp
-    }ms. API Latency is ${Math.round(client.ws.ping)}ms.`,
+      `Pong! Latency is ${
+          reply.createdTimestamp -
+          interaction.createdTimestamp}ms. API Latency is ${
+          Math.round(client.ws.ping)}ms.`,
   );
 };
 
 exports.commandData = {
-  name: "ping",
-  description: "Pongs when pinged.",
-  options: [],
-  defaultPermission: true,
+  name : "ping",
+  description : "Pongs when pinged.",
+  options : [],
+  defaultPermission : true,
 };
 
 // Set guildOnly to true if you want it to be available on guilds only.
 // Otherwise false is global.
 exports.conf = {
-  permLevel: "User",
-  guildOnly: false,
+  permLevel : "User",
+  guildOnly : false,
 };
