b4f374c98846a5178233e91ad1fe95eb608ce6c1
Restyled.io <commits@restyled.io>
2024-03-21 14:18:14 +0000

    Restyled by clang-format

--- commands/stats.js
+++ commands/stats.js
@@ -1,22 +1,24 @@
-const { version } = require("discord.js");
+const {version} = require("discord.js");
 // const { codeBlock } = require("@discordjs/builders");
-const { DurationFormatter } = require("@sapphire/time-utilities");
+const {DurationFormatter} = require("@sapphire/time-utilities");
 const durationFormatter = new DurationFormatter();
 
 exports.run = (client, message, args, level) => {
   // eslint-disable-line no-unused-vars
   const duration = durationFormatter.format(client.uptime);
   const stats = codeBlock(
-    "asciidoc",
-    `= STATISTICS =
-  • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(
-    2,
-  )} MB
+      "asciidoc",
+      `= STATISTICS =
+  • Mem Usage  :: ${
+          (process.memoryUsage().heapUsed / 1024 / 1024)
+              .toFixed(
+                  2,
+                  )} MB
   • Uptime     :: ${duration}
-  • Users      :: ${client.guilds.cache
-    .map((g) => g.memberCount)
-    .reduce((a, b) => a + b)
-    .toLocaleString()}
+  • Users      :: ${
+          client.guilds.cache.map((g) => g.memberCount)
+              .reduce((a, b) => a + b)
+              .toLocaleString()}
   • Servers    :: ${client.guilds.cache.size.toLocaleString()}
   • Channels   :: ${client.channels.cache.size.toLocaleString()}
   • Discord.js :: v${version}
@@ -26,15 +28,15 @@ exports.run = (client, message, args, level) => {
 };
 
 exports.conf = {
-  enabled: false,
-  guildOnly: false,
-  aliases: [],
-  permLevel: "User",
+  enabled : false,
+  guildOnly : false,
+  aliases : [],
+  permLevel : "User",
 };
 
 exports.help = {
-  name: "stats",
-  category: "Miscellaneous",
-  description: "Gives some useful bot statistics",
-  usage: "stats",
+  name : "stats",
+  category : "Miscellaneous",
+  description : "Gives some useful bot statistics",
+  usage : "stats",
 };
