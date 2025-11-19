4a9c4910ad6d18eb8bbd528763364cfb16573a17
Biagio <biagio.santori@gmail.com>
2025-11-19 21:52:44 +0000

    Change log level from 'ready' to 'clientReady'
    Signed-off-by: Biagio <biagio.santori@gmail.com>

--- events/ready.js
+++ events/ready.js
@@ -31,6 +31,6 @@ module.exports = async (client) => {
     `${client.user.tag}, ready to serve ${client.guilds.cache
       .map((g) => g.memberCount)
       .reduce((a, b) => a + b)} users in ${client.guilds.cache.size} servers.`,
-    "ready",
+    "clientReady",
   );
 };
